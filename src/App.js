dd725bbaacba94e52b491c29c419c3543ec460d3
Elvis <elvissamir18@gmail.com>
2022-04-21 17:56:42 -0400

    Change App from function component to class component

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import Footer from './components/Footer.jsx'
 import Product from './components/Product';
 import ProductPage from './components/ProductPage';
 import NotFound from './components/NotFound';
+import { Component } from 'react';
 
-function App() { 
-  return (
-    <div className="App">
-      <div className='app-container'>
-        <div className='app-nav'>
-            < Nav />
-        </div>
-
-        <div className='app-content'>
-            <Routes>
-                <Route path='/' element={ <ProductPage /> } />
-                <Route path='/movies/:id' element={<Product />} />
-                <Route path='*' element={ <NotFound /> } />
-            </Routes>
-        </div>
-
-        <div className='app-footer'>
-          <Footer />
-        </div>
-      </div>
-    </div>
-  );
+class App extends Component { 
+    render() {
+        return (
+          <div className="App">
+            <div className='app-container'>
+              <div className='app-nav'>
+                  < Nav />
+              </div>
+      
+              <div className='app-content'>
+                  <Routes>
+                      <Route path='/' element={ <ProductPage /> } />
+                      <Route path='/products/product' element={<Product />} />
+                      <Route path='*' element={ <NotFound /> } />
+                  </Routes>
+              </div>
+      
+              <div className='app-footer'>
+                <Footer />
+              </div>
+            </div>
+          </div>
+        );
+    }
 }
 
 export default App;
