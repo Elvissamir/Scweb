56a891833220803b3e8a156dfaf09b8c769b6615
Elvis <elvissamir18@gmail.com>
2022-04-22 11:30:12 -0400

    Add Nav component styles and content

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 
 class Nav extends Component {
     render() { 
-        return (<p>Nav</p>);
+        return (
+            <div className='nav-container'>
+                <nav className='nav'>
+                    <div className='nav-content'>
+                        <ul className='menu'>
+                            <li className='menu-link'>WOMEN</li>
+                            <li className='menu-link'>MEN</li>
+                            <li className='menu-link'>KIDS</li>
+                        </ul>
+
+                        <div className="logo">LOGO</div>
+
+                        <div className="options-menu">
+                            <p>$</p>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        );
     }
 }
  
