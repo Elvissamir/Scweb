177ea79c74306da466ebaee13e28edbf4a821605
Elvis <elvissamir18@gmail.com>
2022-04-23 16:07:43 -0400

    Use apollo client in App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { ApolloProvider, ApolloClient, gql, InMemoryCache } from '@apollo/client';
 import { Route, Routes } from 'react-router-dom';
 import Nav from './components/layout/Nav'
 import Footer from './components/layout/Footer'
@@ -6,28 +7,42 @@ import Product from './components/products/Product';
 import ProductPage from './components/products/ProductPage';
 import NotFound from './components/NotFound';
 
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_SERVER_URI,
+  cache: new InMemoryCache()
+})
+
+const product_query = gql`
+  query fetchCategories {
+    categories {
+      name
+    }
+  }
+`
 class App extends Component { 
     render() {
         return (
-          <div className="App">
-            <div className='app-container'>
-              <div className='app-nav'>
-                  < Nav />
-              </div>
-      
-              <div className='app-content'>
-                  <Routes>
-                      <Route path='/' element={ <ProductPage /> } />
-                      <Route path='/products/product' element={<Product />} />
-                      <Route path='*' element={ <NotFound /> } />
-                  </Routes>
-              </div>
-      
-              <div className='app-footer'>
-                <Footer />
+          <ApolloProvider client={client}>   
+            <div className="App">
+              <div className='app-container'>
+                <div className='app-nav'>
+                    < Nav />
+                </div>
+        
+                <div className='app-content'>
+                    <Routes>
+                        <Route path='/' element={ <ProductPage /> } />
+                        <Route path='/products/product' element={<Product />} />
+                        <Route path='*' element={ <NotFound /> } />
+                    </Routes>
+                </div>
+        
+                <div className='app-footer'>
+                  <Footer />
+                </div>
               </div>
             </div>
-          </div>
+          </ApolloProvider>
         );
     }
 }
--- src/components/products/ProductPage.jsx
+++ src/components/products/ProductPage.jsx
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 class ProductPage extends Component {
 
     async componentDidMount() {
-
         try {
            
         }
