617bdc0f66d8d439121a5b0f268aa3cf14109080
Elvissamir <elvissamir18@gmail.com>
2022-04-24 15:10:16 -0400

    Add CartPage in views

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import { ApolloProvider } from '@apollo/client';
+import client from './http/apolloClient'
 import Nav from './components/layout/Nav'
 import Footer from './components/layout/Footer'
 import Router from './components/Router';
@@ -7,19 +9,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='app-container'>
-          <div className='app-nav'>
-              < Nav />
+        <ApolloProvider client={client}>
+          <div className='app-container'>
+            <div className='app-nav'>
+              <Nav />
+            </div>
+            <div className='app-content'>
+              <Router />
+            </div>
+            <div className='app-footer'>
+              <Footer />
+            </div>
           </div>
-
-          <div className='app-content'>
-            <Router />
-          </div>
-
-          <div className='app-footer'>
-            <Footer />
-          </div>
-        </div>
+        </ApolloProvider>
       </div>
     );
   }
--- src/views/ProductPage.jsx
+++ src/views/ProductPage.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class ProductPage extends Component {
-
     async componentDidMount() {
         try {
            
