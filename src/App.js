15dc33537fef8f445e137dfa0cc5a0ea9e43e675
Elvissamir <elvissamir18@gmail.com>
2022-04-24 16:46:24 -0400

    Remove ApolloProvider component from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-import { ApolloProvider } from '@apollo/client';
-import client from './services/http/apolloClient'
 import Nav from './components/layout/Nav'
 import Footer from './components/layout/Footer'
 import Router from './components/Router';
@@ -9,19 +7,17 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <ApolloProvider client={client}>
-          <div className='app-container'>
-            <div className='app-nav'>
-              <Nav />
-            </div>
-            <div className='app-content'>
-              <Router />
-            </div>
-            <div className='app-footer'>
-              <Footer />
-            </div>
+        <div className='app-container'>
+          <div className='app-nav'>
+            <Nav />
           </div>
-        </ApolloProvider>
+          <div className='app-content'>
+            <Router />
+          </div>
+          <div className='app-footer'>
+            <Footer />
+          </div>
+        </div>
       </div>
     );
   }
