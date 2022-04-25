1ff569f6c4199abaebcf6283438e0e6870211e2a
Elvissamir <elvissamir18@gmail.com>
2022-04-25 07:53:27 -0400

    Add renderBlock, renderContent, renderNoDataBlock and renderErrorBlock and additional state properties to ProductListingPage.

--- src/views/ProductListingPage.jsx
+++ src/views/ProductListingPage.jsx
@@ -1,27 +1,57 @@
 import React, { Component } from 'react';
 import { findCategories } from '../services/http/ProductListing/ProductListingPage'
+import CategoriesBar from '../components/CategoriesBar';
 
 class ProductPage extends Component {
-    state = { categories: null }
+    state = { 
+        categories: [], 
+        loading: true,
+        hasData: false,
+        error: false
+    }
 
     async componentDidMount() {
         const result = await findCategories()
         if (!result.error) {
             const categories = result.categories.map(category => category.name)
-            this.setState({ categories })
+            this.setState({ categories, hasData: true, loading: false })
+        }
+        else {
+            this.setState({ error:true, loading: false })
         }
     }
 
-    // Add handleCategoryChange
+    renderBlock() {
+        if (!this.state.error) {
+            if (!this.state.hasData)
+                return this.renderNoDataBlock()
+            return this.renderContent()
+        }
 
+        return this.renderErrorBlock()
+    }
+
+    renderContent() {
+        return (
+            <CategoriesBar categories={this.state.categories} />
+        )
+    }
+
+    renderNoDataBlock() {
+        return <div>There are no items to show at the moment.</div>
+    }
+
+    renderErrorBlock() {
+        return <div>Something happened. Try again in a few minutes.</div>
+    }
+
+    // Add handleCategoryChange
     render() { 
         return (
-            <>
-                <div className='categories-container'>
-
-                </div>
-            </>
-        );
+            <div className='content-wrapper'>
+                { this.renderBlock() }
+            </div>
+        )
     }
 }
  
