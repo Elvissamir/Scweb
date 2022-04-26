8b286df00b68e2196fa56075dae02288fc772ed2
Elvissamir <elvissamir18@gmail.com>
2022-04-26 13:12:09 -0400

    Add Select active link in Nav component

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
+    state = { activeIndex: 0, links: ['WOMEN', 'MEN', 'KIDS'] }
+
+    isActive = (index) => {
+        return index === this.state.activeIndex
+    }
+
+    handleLinkSelect = ({ target }) => {
+        const index = target.value
+        if (index != this.state.activeIndex)
+            this.setState({ activeIndex: index })
+    }
+
     render() { 
         return (
             <div className='nav-container'>
                 <nav className='nav'>
                     <div className='nav-content'>
                         <ul className='menu'>
-                            <li className='menu-link'>WOMEN</li>
-                            <li className='menu-link'>MEN</li>
-                            <li className='menu-link'>KIDS</li>
+                            {this.state.links.map((link, index) => 
+                                <li 
+                                    value={index}
+                                    key={index} 
+                                    onClick={ this.handleLinkSelect }
+                                    className={this.isActive(index)? 'menu-link active-link': 'menu-link'}>
+                                    { link }
+                                </li>
+                            )}
                         </ul>
                         <div className="logo-container">
                             <img src="/imgs/logo.svg" alt="logo" />
