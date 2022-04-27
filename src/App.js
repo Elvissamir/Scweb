7ab8466a2f0bea3d82eeec8a1b4bebb41aa1bd0c
Elvissamir <elvissamir18@gmail.com>
2022-04-27 07:20:22 -0400

    Remove CategoriesBar component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,31 @@ import Nav from './components/layout/Nav'
 import Footer from './components/layout/Footer'
 import Router from './components/Router';
 import { findCategories } from './services/http/ProductListing/ProductListingPage'
+import { Provider } from 'react-redux';
 
 class App extends Component { 
-  
+
   async componentDidMount() {
-    const result = await findCategories()
-    if (!result.error) {
-        const categories = result.categories.map(category => category.name.toUpperCase())
-        this.setState({ categories, hasData: true, loading: false, activeCategory: categories[0] })
-    }
-    else {
-        this.setState({ error:true, loading: false })
-    }
+   
   }
 
   render() {
     return (
-      <div className="App">
-        <div className='app-container'>
-          <div className='app-nav'>
-            <Nav />
-          </div>
-          <div className='app-content'>
-            <Router />
-          </div>
-          <div className='app-footer'>
-            <Footer />
-          </div>
+      <Provider store={store}>
+        <div className="App">
+            <div className='app-container'>
+              <div className='app-nav'>
+                <Nav />
+              </div>
+              <div className='app-content'>
+                <Router />
+              </div>
+              <div className='app-footer'>
+                <Footer />
+              </div>
+            </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
