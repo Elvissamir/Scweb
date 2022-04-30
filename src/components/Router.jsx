0508b0bef45dcd6b51b19240765dfa01fb754e41
Elvissamir <elvissamir18@gmail.com>
2022-04-30 17:19:44 -0400

    Add route redirect from / to /all.

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,13 +1,15 @@
 import { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import ProductListingPage from '../views/ProductListingPage';
 import NotFound from '../views/NotFoundPage';
+import routes from '../routes';
 
 class Router extends Component {
     render () {
         return (
             <Routes>
-                <Route path='/' element={ <ProductListingPage /> } />
+                <Route path='/' element={<Navigate to={`${routes.categories}/all`} replace />}/>
+                <Route path={`${routes.categories}/:category`} element={ <ProductListingPage /> } />
                 <Route path='*' element={ <NotFound /> } />
             </Routes>
         )
