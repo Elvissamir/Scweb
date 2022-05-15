d69004f68c3088ec4dd202089bbbcf5df6b52f05
Elvissamir <elvissamir18@gmail.com>
2022-05-15 14:41:40 -0400

    Change cartSlice reducers name to addCartItem and removeCartItem. Change addCartItem content: Now the function checks if the item to add already exists in the cart and updates the count property.

--- src/store/features/cart/cartSlice.js
+++ src/store/features/cart/cartSlice.js
@@ -9,8 +9,12 @@ export const cartSlice = createSlice({
     initialState,
     reducers: {
         addCartItem: (state, action) => {
-            console.log(action.payload)
-            state.items.push(action.payload.item)
+            const cartItem = state.items.find(item => item.id === action.payload.item.id)
+
+            if (!cartItem)
+                state.items.push(action.payload.item)
+            else 
+                cartItem.count += 1
         },
         removeCartItem: (state, action) => {
             state.items.filter(item => action.payload.id !== item.id)
--- src/utils/mapProductToCartItem.js
+++ src/utils/mapProductToCartItem.js
@@ -1,6 +1,6 @@
 const mapProductToCartItem = product => {
     const item = {...product}
-    item.index = null
+    
     item.count = 1
     return item
 }
